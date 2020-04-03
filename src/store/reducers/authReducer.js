b18a40df568b0403f3e75685f444a98d99b27490
kshashwat007 <kshashwat007@gmail.com>
2020-04-03 20:14:03 +0400

    signin and signout using firebase auth working

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,28 @@
-const initState = {}
+const initState = {
+    authError: null
+}
 
 const authReducer = (state = initState, action) => {
-    return state
+    switch(action.type){
+        case 'LOGIN_ERROR':
+            console.log("Login failure")
+            return {
+                ...state,
+                authError: 'Login failed'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log("Login success")
+            return{
+                ...state,
+                authError: null
+            }
+        case 'SIGNOUT_SUCCESS':
+            console.log("Signout Success")
+            return state;
+        default:
+            return state;
+    }
+ 
 }
 
 export default authReducer
